928db9a5dc2fb9813ee85dea36b8749378bf044d
Clint Herron <cherron@apterainc.com>
2020-04-23 12:57:43 -0400

    Fixing not-equals operations in Lua, but still getting an error when loading.

--- data/SuperMarioKart-Snes/script.lua
+++ data/SuperMarioKart-Snes/script.lua
@@ -1,7 +1,7 @@
 
 function p1_lap_score_with_checkpoints ()
   local correct_lap = 0
-  if data.P1Lap != 127 then
+  if data.P1Lap ~= 127 then
     correct_lap = data.P1Lap + 1
   end
   return data.P1LapCheckpoint + (correct_lap * 100)
@@ -9,7 +9,7 @@ end
 
 function p2_lap_score_with_checkpoints ()
   local correct_lap = 0
-  if data.P2Lap != 127 then
+  if data.P2Lap ~= 127 then
     correct_lap = data.P2Lap + 1
   end
   return data.P2LapCheckpoint + (correct_lap * 100)
